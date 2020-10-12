131ec0d741fbaa1284f82fa69ada64c02c3a8d4d
Vladislav Lebedev <tonknaf@gmail.com>
2020-10-12 20:42:01 +0300

    Initial

---
+++ Nuxt.js/pages/forum/_.vue
@@ -0,0 +1,74 @@
+<!--
+pages/<PATH PREFIX>/_.vue
+
+We should process all nested routes,
+so the page path for Nuxt.js should be
+pages/<PATH PREFIX>/_.vue as described in https://nuxtjs.org/guide/routing/#unknown-dynamic-nested-routes
+-->
+<template>
+  <div>
+    <h1>Forum!</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-if="error">Error...</div>
+    <div ref="peerboardContainer"></div>
+  </div>
+</template>
+
+<script>
+const pathPrefix = '<PATH_PREFIX>';
+const boardId = '<BOARD ID>';
+
+export default {
+
+  data() {
+    return {
+      error: '',
+      loading: true,
+    }
+  },
+
+  methods: {
+    async createPeerBoard() {
+      // If you are using Nuxt.js ssr you shouldn't create the forum during server-side rendering
+      // You can use https://nuxtjs.org/faq/window-document-undefined/
+      // or https://nuxtjs.org/api/components-client-only/
+      if (!process.client) {
+        return;
+      }
+
+      const { createForum } = require('@peerboard/core');
+
+      const jwtToken = await http.post('https://api.myapp.com/create-token-for-peerboard');
+
+      const options = {
+        prefix: pathPrefix,
+
+        // Recommended setting so that the forum container
+        // will always occupy all available space
+        minHeight: window.innerHeight,
+
+        // Update your page title according to the forum state
+        onTitleChanged: title =>
+          window.document.title = 'Your title ' + title,
+
+        onReady: () => {
+          // Here you can safely any loading indicators
+          this.loading = false;
+        },
+
+        onFail: () => {
+          this.error = 'Failed to load forum';
+          this.loading = false;
+        },
+      };
+
+      createForum(boardId, this.$refs.peerboardContainer, options)
+    }
+  },
+
+  mounted() {
+    this.createPeerBoard();
+  }
+}
+</script>
+
