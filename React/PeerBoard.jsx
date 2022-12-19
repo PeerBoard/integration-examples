9e26352ce7cdd85e7b56e11190a77501a644beed
sentiens <tonknaf@gmail.com>
2022-12-19 19:01:02 +0300

    Update PeerBoard.jsx

--- React/PeerBoard.jsx
+++ React/PeerBoard.jsx
@@ -1,73 +1,95 @@
-import React from 'react';
-import { createForum } from '@peerboard/core';
+import React from "react";
+import PeerBoard from "@peerboard/core";
 
-// Settings -> Hosting -> Board ID
-const boardID = '<BOARD ID>';
-// Settings -> Hosting -> Path Prefix
-const pathPrefix = '<PATH PREFIX>';
+function CommunityPage() {
+  const container = React.useRef(null);
+  const [jwtToken, setJwtToken] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
 
-// Somewhere in your routing configuration. Don't use exact attribute - you should allow all nested routes
-// <Route path="<PATH PREFIX>" component={PageWithPeerboard} />
+  React.useEffect(() => {
+    setLoading(true);
+    // Optionally, get the token to seamlessly authenticate the user.
+    // Note that this is pseudocode, the exact implementation depends on the http(http, axios, graphql) library and your backend.
+    // If you have SSR we suggest to inject the token on page render to improve loading performance.
+    const jwtTokenReq = http.post('https://myapp.com/api/secure-create-token-for-peerboard').
+      then(res => {
+        if (res.statusIsOK()) {
+          setJwtToken(res.jwtToken);
+          setError(null);
+        } else {
+          throw new Error(res.error);
+        }
+      }).
+      catch(err => {
+        setError(err);
+      });
 
-class PageWithPeerboard extends React.Component {
+    return function cleanup() {
+      // https://dev.to/viclafouch/cancel-properly-http-requests-in-react-hooks-and-avoid-memory-leaks-pd7
+      jwtTokenReq.cancel();
+    };
+  }, []);
 
-  containerRef = React.createRef();
+  // Run effect on component render
+  React.useEffect(() => {
+    if (!jwtToken) {
+      return;
+    }
+    setLoading(true);
 
-  state = {
-    forum: null,
-    error: null,
-    loading: true,
-  };
+    // Settings -> Hosting -> Path prefix
+    const prefix = PREFIX_FROM_SETTINGS;
+    // Settings -> Hosting -> Board ID
+    const communityID = BOARD_ID_FROM_SETTINGS;
 
-  async createPeerBoard() {
     const options = {
       jwtToken,
+      prefix,
 
-      prefix: pathPrefix,
-
-      // Recommended setting so that the forum container
+      // Optional. Recommended setting so that the forum container
       // will always occupy all available space
-      minHeight: window.innerHeight,
+      // replace HEADER_HEIGHT and FOOTER_HEIGHT with your values
+      // Check that window is defined for SSR cases
+      // By default 80% of the screen height is used.
+      minHeight: typeof window !== 'undefined' ?
+        window.innerHeight - HEADER_HEIGHT - FOOTER_HEIGHT : undefined,
 
       // Update your page title according to the forum state
       onTitleChanged: title =>
-        window.document.title = 'Your title ' + title,
-      
-      // You can remove the loaded and forum will be showed to the user
-      onReady: () => this.setState({
-        loading: false,
-      }),
-  
-      onFail: () => this.setState({
-        error: "Failed to load forum...",
-      }),
+        window.document.title = "Your app community: " + title,
     };
-    
-    // Optional. Do this only if you want to implement seamless login https://community.peerboard.com/post/183197627?category=2097967386
-    // Add auth token so the user will be authenticated when community is loaded
-    options.jwtToken = await http.post('https://api.myapp.com/create-token-for-peerboard')
-    
-    this.forum = createForum(boardID, this.containerRef.current, options);
-  }
 
-  componentDidMount() {
-    this.createPeerBoard().catch(err => this.setState({
-      error: err.message,
-    }));
-  }
-  
-  componentWillUnmount() {
-    this.forum.destroy()
-  }
+    // save the promise to use in cleanup
+    const promise = PeerBoardSDK.createForum(communityID, container.current, options).
+      then(() => {
+        // Here you can remove any loading indicators
+        setLoading(false);
+      }).
+      catch(err => {
+        // Show error to the user, report to sentry
+        setLoading(false);
+        setError(err);
+      });
 
-  render() {
-    return (
-      <div>
-        {this.state.error && this.state.error}
-        {this.state.loading && 'Loading...'}
-        <div ref={this.containerRef}></div>
-      </div>
-    );
-  }
+    // We're suggesting to do cleanup everything the page dynamically, for example, on component unmount in React
+    // We support calling createForum in SSR is safe, in that case null will be returned, so you need to check that
+    return function cleanup() {
+      promise.then(api => {
+        if (api) {
+          api.destroy();
+        }
+      });
+    };
+  }, [jwtToken]); // Don't forget to specify jwtToken as dependency
+
+  return (
+    <div>
+      {loading && 'Loading... We suggest using beautiful spinner'}
+      {error && <div>Something went wrong. Please try later</div>}
+      <div ref={container}></div>
+    </div>
+  );
 }
 
+export default CommunityPage;
